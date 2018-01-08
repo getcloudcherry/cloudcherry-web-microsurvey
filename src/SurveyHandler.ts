3fb0b8c4b70fcfd2f4d9b701d5bd5a3ddf14b5fe
Abhishek Yagnik <abhishekyagnik@yahoo.com>
2018-01-08 11:33:45 +0530

    fixed bugs

--- src/SurveyHandler.ts
+++ src/SurveyHandler.ts
@@ -733,7 +733,16 @@ class SurveyHandler {
             for (let initial = startRange; initial <= endRange; initial += divider) {
               options += '<span data-rating="' + initial + '" class="option-number-item option-scale '+scaleVisibility+'" style="'+optionStyle+'">' + initial + '</span>';
             }
+            if ((endRange-startRange+1) < 5) {
+              var oleft = document.querySelectorAll(".option-left-rating-text")[0];
+              var oright = document.querySelectorAll(".option-right-rating-text")[0];
+              var optionLeftExtraClass = 'option-left-rating-text-small-container';
+              var optionRightExtraClass = 'option-right-rating-text-small-container';
+
+            }
             questionTemplate = questionTemplate.replace("{{optionsRange}}", options);
+            questionTemplate = questionTemplate.replace(/{{optionLeftExtraClass}}/g, optionLeftExtraClass);
+            questionTemplate = questionTemplate.replace(/{{optionRightExtraClass}}/g, optionRightExtraClass);
             questionTemplate = questionTemplate.replace("{{leftLabel}}", startRangeLabel);
             questionTemplate = questionTemplate.replace("{{rightLabel}}", endRangeLabel);
             questionTemplate = questionTemplate.replace(/{{mobileImageUrl}}/g, mobileImageUrl);
