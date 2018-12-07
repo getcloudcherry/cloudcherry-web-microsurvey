91288094f259dc8fbbb8d9625a23a1dc2313ac47
Vinay <vinay@getcloudcherry.com>
2018-12-07 16:50:52 +0530

    No Survey token and token with space handling

--- src/Survey.ts
+++ src/Survey.ts
@@ -237,10 +237,10 @@ class Survey {
       let event = new CustomEvent( Constants.SURVEY_DATA_EVENT + "-" + self.surveyToken, { detail: JSON.parse( JSON.stringify( surveyData ) ) } );
       document.dispatchEvent( event );
       self.dom.hideLoader();
-      if(self.surveyData) {
+      if ( self.surveyData ) {
         self.initSurveyQuestions();
       } else {
-        self.survey.displayWelcomeQuestion("The Survey has been expired");
+        self.survey.displayWelcomeQuestion( "The Survey has been expired" );
       }
     };
     let errorcb = null;
@@ -260,8 +260,9 @@ class Survey {
     // self.survey.attachSurvey(this.surveyData);
     // self.config.language = "हिन्दी";
     self.config.language = "default";
-
-    self.survey.displayWelcomeQuestion();
+    if ( self.surveyToken && decodeURIComponent( self.surveyToken ).trim() !== '' ) {
+      self.survey.displayWelcomeQuestion();
+    }
     self.dom.setupListeners();
     // self.survey.displayLanguageSelector();
     //survey start event.
