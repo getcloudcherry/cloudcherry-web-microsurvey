6bfefe765b255ba4df2f79aa01e231750ba6504d
venkat <venkat@getcloudcherry.com>
2019-03-29 21:09:07 +0530

    support for date question

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export type PrefillsBatchOrSingle = Array<PrefillDictionary | string | ResponseType>;
+
+export type ResponseType = string | number;
+
+export type PrefillType = 'DIRECT' | 'BY_TAG' | 'BY_NOTE';
+
+export interface PrefillDictionary {
+  [ key: string ]: ResponseType
+}
