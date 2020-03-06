7d0adeb1ff71b82673acbc58d26304939e3f84b0
Vinay P.(vinayp2) <vinayp2@cisco.com>
2020-03-06 11:30:42 +0530

    Fix: Check for isActive

--- src/CCSDKEntry.ts
+++ src/CCSDKEntry.ts
@@ -1,7 +1,7 @@
-import { Cookie } from './helpers/Cookie';
+import { Cookie } from "./helpers/Cookie";
 import { SurveyManager } from "./SurveyManager";
 import { Survey } from "./Survey";
-import { PrefillsBatchOrSingle, PrefillDictionary } from './types';
+import { PrefillsBatchOrSingle, PrefillDictionary } from "./types";
 
 /**
  * functions that are exposed to SDK User are written here.
@@ -27,17 +27,17 @@ let localCCSDK = {
 (window as any).ccsdkDebug = false;
 (window as any).isMobile = window.innerWidth <= 768 ? true : false;
 
-if (typeof (window as any).CCSDK !== 'undefined') {
+if (typeof (window as any).CCSDK !== "undefined") {
   var queue = (window as any).CCSDK.q;
-  (window as any).CCSDK = function () {
+  (window as any).CCSDK = function() {
     if (arguments && arguments.length == 0) {
       var time = new Date();
     } else {
-      (window as any).ccsdkDebug ? console.log(arguments) : '';
+      (window as any).ccsdkDebug ? console.log(arguments) : "";
       var args = (Array as any).from(arguments);
       //Call this functions as ccsdk('functionName', ['argument1', 'argument2']);
       var functionName = args.splice(0, 1)[0];
-      (window as any).ccsdkDebug ? console.log(functionName) : '';
+      (window as any).ccsdkDebug ? console.log(functionName) : "";
       return localCCSDK[functionName].apply(this, args);
     }
   };
@@ -48,8 +48,8 @@ if (typeof (window as any).CCSDK !== 'undefined') {
       localCCSDK[functionName].apply(this, args);
     }
   }
-  let eventCCReady: Event = document.createEvent('Event');
-  eventCCReady.initEvent('ccready', true, true);
+  let eventCCReady: Event = document.createEvent("Event");
+  eventCCReady.initEvent("ccready", true, true);
   document.dispatchEvent(eventCCReady);
 }
 
@@ -57,47 +57,67 @@ export function init(surveyToken: any) {
   //config options can be set in arguments[1]
   //available config options : themeColor
   surveyToken = decodeURIComponent(surveyToken).trim();
-  let config = (typeof arguments[1] === 'object') ? arguments[1] : {};
+  let config = typeof arguments[1] === "object" ? arguments[1] : {};
   //create survey instance
-  if (typeof Cookie.get(surveyToken + '-finish') !== 'undefined' && Cookie.get(surveyToken + '-finish')) {
+  if (
+    typeof Cookie.get(surveyToken + "-finish") !== "undefined" &&
+    Cookie.get(surveyToken + "-finish")
+  ) {
     return;
   }
-  if (typeof Cookie.get(surveyToken + '-coolDown') !== 'undefined' && Cookie.get(surveyToken + '-coolDown')) {
+  if (
+    typeof Cookie.get(surveyToken + "-coolDown") !== "undefined" &&
+    Cookie.get(surveyToken + "-coolDown")
+  ) {
     return;
   }
 
-  if (typeof config.isActive !== 'undefined' && config.isActive) {
-    SurveyManager.surveyInstances[surveyToken] = (SurveyManager.surveyInstances[surveyToken]) ? SurveyManager.surveyInstances[surveyToken] : new Survey(surveyToken, config);
-    SurveyManager.surveyInstances[surveyToken].tracking.trackEvent('Init MicroSurvey', {
-      token: surveyToken,
-      data: {
-        name: (<any>window).isMobile ? 'Mobile Mode' : 'Desktop Mode',
-        action: surveyToken
-      }
-    }, null, null);
+  if (config && typeof config.isActive !== "undefined" && config.isActive) {
+    SurveyManager.surveyInstances[surveyToken] = SurveyManager.surveyInstances[
+      surveyToken
+    ]
+      ? SurveyManager.surveyInstances[surveyToken]
+      : new Survey(surveyToken, config);
+    SurveyManager.surveyInstances[surveyToken].tracking.trackEvent(
+      "Init MicroSurvey",
+      {
+        token: surveyToken,
+        data: {
+          name: (<any>window).isMobile ? "Mobile Mode" : "Desktop Mode",
+          action: surveyToken
+        }
+      },
+      null,
+      null
+    );
 
     if (SurveyManager.prefillQueue[surveyToken]) {
       let object = SurveyManager.prefillQueue[surveyToken];
-      SurveyManager.surveyInstances[surveyToken].tracking.trackEvent('Init MicroSurvey', {
-        token: surveyToken,
-        data: {
-          name: 'Prefill lazily',
-          action: surveyToken
-        }
-      }, null, null);
+      SurveyManager.surveyInstances[surveyToken].tracking.trackEvent(
+        "Init MicroSurvey",
+        {
+          token: surveyToken,
+          data: {
+            name: "Prefill lazily",
+            action: surveyToken
+          }
+        },
+        null,
+        null
+      );
       if (object.directPrefills) {
         object.directPrefills.forEach(x => {
-          SurveyManager.surveyInstances[surveyToken].prefill(x, 'DIRECT');
+          SurveyManager.surveyInstances[surveyToken].prefill(x, "DIRECT");
         });
         delete object.directPrefills;
       } else if (object.byTagPrefills) {
         object.byTagPrefills.forEach(x => {
-          SurveyManager.surveyInstances[surveyToken].prefill(x, 'BY_TAG');
+          SurveyManager.surveyInstances[surveyToken].prefill(x, "BY_TAG");
         });
         delete object.byTagPrefills;
       } else if (object.byNotePrefills) {
         object.byTagPrefills.forEach(x => {
-          SurveyManager.surveyInstances[surveyToken].prefill(x, 'BY_NOTE');
+          SurveyManager.surveyInstances[surveyToken].prefill(x, "BY_NOTE");
         });
         delete object.byNotePrefills;
       }
@@ -106,15 +126,16 @@ export function init(surveyToken: any) {
   } else {
     //do nothing
   }
-
 }
 
 export function destroy(surveyToken: string) {
   //remove from trigger manager, delete instance.
-  if (!SurveyManager ||
+  if (
+    !SurveyManager ||
     !SurveyManager.surveyInstances ||
     !SurveyManager.surveyInstances[surveyToken] ||
-    !SurveyManager.surveyInstances[surveyToken].dom) {
+    !SurveyManager.surveyInstances[surveyToken].dom
+  ) {
     return;
   }
   SurveyManager.surveyInstances[surveyToken].dom.destroyListeners();
@@ -123,123 +144,160 @@ export function destroy(surveyToken: string) {
 }
 //
 export function trigger(surveyToken: string, type: string, target: string) {
-  (window as any).ccsdkDebug ? console.log(SurveyManager.surveyInstances) : '';
+  (window as any).ccsdkDebug ? console.log(SurveyManager.surveyInstances) : "";
 
-  if (typeof SurveyManager.surveyInstances[surveyToken] != 'undefined') {
-    SurveyManager.surveyInstances[surveyToken].tracking.trackEvent('Trigger Type', {
-      token: surveyToken,
-      data: {
-        name: type,
-        action: surveyToken
-      }
-    }, null, null);
+  if (typeof SurveyManager.surveyInstances[surveyToken] != "undefined") {
+    SurveyManager.surveyInstances[surveyToken].tracking.trackEvent(
+      "Trigger Type",
+      {
+        token: surveyToken,
+        data: {
+          name: type,
+          action: surveyToken
+        }
+      },
+      null,
+      null
+    );
     SurveyManager.surveyInstances[surveyToken].trigger(type, target);
   }
   //tell trigger manager to register trigger.
 }
 
 export function on(surveyToken: string, type: string, callback: any) {
-  if (typeof SurveyManager.surveyInstances[surveyToken] != 'undefined') {
-    SurveyManager.surveyInstances[surveyToken].tracking.trackEvent('Event Listeners', {
-      token: surveyToken,
-      data: {
-        name: type,
-        action: surveyToken
-      }
-    }, null, null);
+  if (typeof SurveyManager.surveyInstances[surveyToken] != "undefined") {
+    SurveyManager.surveyInstances[surveyToken].tracking.trackEvent(
+      "Event Listeners",
+      {
+        token: surveyToken,
+        data: {
+          name: type,
+          action: surveyToken
+        }
+      },
+      null,
+      null
+    );
     SurveyManager.surveyInstances[surveyToken].on(type, callback);
   }
 }
 
-export function prefill(surveyToken: string, ...restArgs: PrefillsBatchOrSingle) {
-  if (typeof SurveyManager.surveyInstances[surveyToken] != 'undefined') {
+export function prefill(
+  surveyToken: string,
+  ...restArgs: PrefillsBatchOrSingle
+) {
+  if (typeof SurveyManager.surveyInstances[surveyToken] != "undefined") {
     let questionId;
-    if (typeof restArgs[0] === 'string') {
+    if (typeof restArgs[0] === "string") {
       questionId = restArgs[0];
     } else {
-      questionId = Object.keys((<PrefillDictionary>restArgs[0])).length + ' Questions';
+      questionId =
+        Object.keys(<PrefillDictionary>restArgs[0]).length + " Questions";
     }
-    SurveyManager.surveyInstances[surveyToken].tracking.trackEvent('Prefill', {
-      token: surveyToken,
-      data: {
-        name: questionId,
-        action: surveyToken
-      }
-    }, null, null);
-    SurveyManager.surveyInstances[surveyToken].prefill(restArgs, 'DIRECT');
+    SurveyManager.surveyInstances[surveyToken].tracking.trackEvent(
+      "Prefill",
+      {
+        token: surveyToken,
+        data: {
+          name: questionId,
+          action: surveyToken
+        }
+      },
+      null,
+      null
+    );
+    SurveyManager.surveyInstances[surveyToken].prefill(restArgs, "DIRECT");
   } else {
     if (SurveyManager.prefillQueue[surveyToken]) {
       SurveyManager.prefillQueue[surveyToken].directPrefills.push(restArgs);
     } else {
       SurveyManager.prefillQueue[surveyToken] = {
         directPrefills: [restArgs]
-      }
+      };
     }
   }
 }
 
-export function prefillByTag(surveyToken: string, ...restArgs: PrefillsBatchOrSingle) {
-  if (typeof SurveyManager.surveyInstances[surveyToken] != 'undefined') {
+export function prefillByTag(
+  surveyToken: string,
+  ...restArgs: PrefillsBatchOrSingle
+) {
+  if (typeof SurveyManager.surveyInstances[surveyToken] != "undefined") {
     let questionTag;
-    if (typeof restArgs[0] === 'string') {
+    if (typeof restArgs[0] === "string") {
       questionTag = restArgs[0];
     } else {
-      questionTag = Object.keys((<PrefillDictionary>restArgs[0])).length + ' Questions';
+      questionTag =
+        Object.keys(<PrefillDictionary>restArgs[0]).length + " Questions";
     }
-    SurveyManager.surveyInstances[surveyToken].tracking.trackEvent('Prefill by tag', {
-      token: surveyToken,
-      data: {
-        name: questionTag,
-        action: surveyToken
-      }
-    }, null, null);
-    SurveyManager.surveyInstances[surveyToken].prefill(restArgs, 'BY_TAG');
+    SurveyManager.surveyInstances[surveyToken].tracking.trackEvent(
+      "Prefill by tag",
+      {
+        token: surveyToken,
+        data: {
+          name: questionTag,
+          action: surveyToken
+        }
+      },
+      null,
+      null
+    );
+    SurveyManager.surveyInstances[surveyToken].prefill(restArgs, "BY_TAG");
   } else {
     if (SurveyManager.prefillQueue[surveyToken]) {
       SurveyManager.prefillQueue[surveyToken].byTagPrefills.push(restArgs);
     } else {
       SurveyManager.prefillQueue[surveyToken] = {
         byTagPrefills: [restArgs]
-      }
+      };
     }
   }
 }
 
-export function prefillByNote(surveyToken: string, ...restArgs: PrefillsBatchOrSingle) {
-  if (typeof SurveyManager.surveyInstances[surveyToken] != 'undefined') {
+export function prefillByNote(
+  surveyToken: string,
+  ...restArgs: PrefillsBatchOrSingle
+) {
+  if (typeof SurveyManager.surveyInstances[surveyToken] != "undefined") {
     let questionNote;
-    if (typeof restArgs[0] === 'string') {
+    if (typeof restArgs[0] === "string") {
       questionNote = restArgs[0];
     } else {
-      questionNote = Object.keys((<PrefillDictionary>restArgs[0])).length + ' Questions';
+      questionNote =
+        Object.keys(<PrefillDictionary>restArgs[0]).length + " Questions";
     }
-    SurveyManager.surveyInstances[surveyToken].tracking.trackEvent('Prefill by Note', {
-      token: surveyToken,
-      data: {
-        name: questionNote,
-        action: surveyToken
-      }
-    }, null, null);
-    SurveyManager.surveyInstances[surveyToken].prefill(restArgs, 'BY_NOTE');
+    SurveyManager.surveyInstances[surveyToken].tracking.trackEvent(
+      "Prefill by Note",
+      {
+        token: surveyToken,
+        data: {
+          name: questionNote,
+          action: surveyToken
+        }
+      },
+      null,
+      null
+    );
+    SurveyManager.surveyInstances[surveyToken].prefill(restArgs, "BY_NOTE");
   } else {
     if (SurveyManager.prefillQueue[surveyToken]) {
       SurveyManager.prefillQueue[surveyToken].byNotePrefills.push(restArgs);
     } else {
       SurveyManager.prefillQueue[surveyToken] = {
         byNotePrefills: [restArgs]
-      }
+      };
     }
   }
 }
 
 export function show(surveyToken: string) {
-  if (typeof SurveyManager.surveyInstances[surveyToken] != 'undefined') {
+  if (typeof SurveyManager.surveyInstances[surveyToken] != "undefined") {
     SurveyManager.surveyInstances[surveyToken].show();
   }
 }
 
 export function hide(surveyToken: string) {
-  if (typeof SurveyManager.surveyInstances[surveyToken] != 'undefined') {
+  if (typeof SurveyManager.surveyInstances[surveyToken] != "undefined") {
     SurveyManager.surveyInstances[surveyToken].dom.destroyListeners();
     SurveyManager.surveyInstances[surveyToken].destroy();
     SurveyManager.surveyInstances[surveyToken].hide();
